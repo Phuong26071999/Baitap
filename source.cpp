ccaa528a9d34b590ecdfd83ff2bebd6af18903c6
quocbaostu <72492204+quocbaostu@users.noreply.github.com>
2020-10-21 16:39:47 +0700

    update tru random

--- source.cpp
+++ source.cpp
@@ -1,52 +1,81 @@
 #include <iostream>
-using namespace std;
-int tong(int a, int b){
-	int Tong = a + b;
-	return Tong:
+#include <cstdlib>
+#include <ctime>
+#include <math.h>
+
+void TruNgauNhien(int a, int b) {
+	cout << "Luyen tap phep Tru 2 so ngau nhien" << endl;
+	cout << "So nguyen a = " << a << endl;
+	cout << "So nguyen b = " << b << endl;
+	float kq;
+	cout << "Moi ban nhao ket qua phep tru " << a << " - " << b << " = ";
+	cin >> kq;
+	do {
+		if (kq == a - b) {
+			cout << "Ban da nhap dung ket qua phe tinh " << a << " - " << b << " = " << kq;
+		}
+		else {
+			cout << "ban da nhap sai vui long nhap lai ket qua" << endl;
+			cout << "Nhap lai = ";
+			cin >> kq;
+		}
+	} while (kq != a - b);
 }
-int hieu(int a, int b){
-	return (a - b);
-}
-int tich(int a, int b){
-	return a * b;
-}
-float thuong(int a, int b){
-	while (b == 0){
-		cout << "Nhap sai, nhap b khac 0: ";
-		cin >> b;
-	}
-	return (float) a/b;
-}
+
 int main()
 {
-	int a, b;
+	int a, b, kqR;
+	float r, cd, cr, c;
 	char phepToan;
+	srand(time(NULL));
+	int randA = 0 + rand() % (100 + 1 - 0);
+	int randB = 0 + rand() % (100 + 1 - 0);
 	cout << "Nhap 2 so nguyen: ";
 	cin >> a >> b;
-	cout << "Tinh gi? (+,-,*,/) ";
+	//cout << "Tinh gi? (+,-,*,/,c) ";
+	cout << "Tinh gi? (+,-,*,/) " << endl
+		<< "\tr: Tinh cong tu 2 so ngau nhien." << endl
+		<< "\tv: Tinh dien tich hinh vuong." << endl
+		<< "\tn: Tinh dien tich hinh chu nhat." << endl
+		<< "\tt: Tinh dien tich hinh tron." << endl
+		<< "\tx: Tru ngau nhien 2 so." << endl
+		<< "Hay chon: ";
 	cin >> phepToan;
-	
+
 	switch (phepToan)
 	{
 	case '+':
-		cout<<"Tong la: <<tong(a,b)<<endl;
+		cout << "Tong la: " << tong(a, b) << endl;
 		break;
 	case '-':
-		int kq2 = hieu(a,b);
-		cout<<"hieu la : "<< kq2 << endl;
+		cout << "hieu la : " << hieu(a, b) << endl;
 		break;
 	case '*':
-		int kq;
-		kq = tich(a,b);
-		cout << "Tich cua " << a << "x" << b << " = " << kq;
+		cout << "Tich cua " << a << "x" << b << " = " << tich(a, b) << endl;
 		break;
 	case '/':
-		float kq4 = thuong(a,b);
-		cout << "Ket qua phep tinh chia giua " << a << "va" << b << "la: " << kq4;
+		cout << "Ket qua phep tinh chia giua " << a << "va" << b << "la: " << thuong(a, b) << endl;
+		break;
+	case 'r':
+		cout << "\t\tLuyen tap phep cong tu 2 so ngau nhien." << endl << endl;
+		cout << "\tSo nguyen a = " << randA << endl;
+		cout << "\tSo nguyen b = " << randB << endl;
+		cout << "\tKet qua " << randA << " + " << randB << " = ";
+		cin >> kqR;
+		if (kqR == (randA + randB)) {
+			cout << "Ban da nhap dung ket qua la: " << randA + randB << " !!!" << endl;
+		}
+		else
+			cout << "Ban da nhap sai ket qua!!!" << endl;
+		break;
+	case 'x':
+		TruNgauNhien(randA,randB);
 		break;
 	default:
 		cout << "Chon sai phep toan";
 		break;
 	}
+
 	return 0;
+
 }
